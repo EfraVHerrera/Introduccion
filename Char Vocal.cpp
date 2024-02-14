af3c7ee28a3527d4e389f19883476bc6f678aa77
EfraVHerrera <159298473+EfraVHerrera@users.noreply.github.com>
2024-02-14 14:18:54 -0400

    Add files via upload

---
+++ Char Vocal.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int Vocales(char *pc) {
+	int contador = 0;
+	
+	while (*pc != '\0') {
+		
+		if (*pc == 'a' || *pc == 'e' || *pc == 'i' || *pc == 'o' || *pc == 'u' ||
+			*pc == 'A' || *pc == 'E' || *pc == 'I' || *pc == 'O' || *pc == 'U') {
+			contador++;
+		}
+		pc++;
+	}
+	
+	return contador;
+}
+
+int main(int argc, char *argv[]){
+	char cadena[] = "HOLA MUNDO";
+	char *pc = cadena;
+	
+	// Contar vocales de la funcion
+	int numVocales = Vocales(pc);
+	
+	cout << "El número de vocales: " << numVocales << endl;
+	
+	return 0;
+}
+	
