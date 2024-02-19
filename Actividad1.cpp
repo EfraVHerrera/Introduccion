0418346afaec17c5f253b85f80ad91fa9c8d9b75
EfraVHerrera <159298473+EfraVHerrera@users.noreply.github.com>
2024-02-19 13:39:56 -0400

    Update Actividad1.cpp

--- Actividad1.cpp
+++ Actividad1.cpp
@@ -5,7 +5,7 @@
 
 using namespace std;
 
-//DEFINICION DE ESTRUTURA
+// Definición de las estructuras
 struct LIBRO {
 	string title;
 	string author;
@@ -27,76 +27,96 @@ struct BIBLIOTECARIO {
 	string email;
 };
 
-//FUNCION ALEATORIA
+const int NUM_LIBROS = 7;
+const int NUM_USUARIOS = 7;
+const int NUM_BIBLIOTECARIOS = 7;
+
+// Funciones para generar datos aleatorios
 string getRandomElement(string arr[], int size) {
 	return arr[rand() % size];
 }
 
 int getRandomYear() {
-	return rand() % 50 + 1970; //A�OS ALEATORIOS 1970 y 2019
+	return rand() % 50 + 1970; // Años aleatorios entre 1970 y 2019
 }
 
 string generateEmail(string name, string lastname) {
 	return name + "." + lastname + "@upds.net.bo";
 }
 
-int main() {
-	//SEMILLA PARA NUMERO ALEATORIOS
-	srand(time(0));
-	
-	//ARRAYS FIJOS DE DATOS
-	string listaLibros[] = {"Aprende C++", "Aprende PHP", "Aprende Linux"};
-	int numLibros = sizeof(listaLibros) / sizeof(listaLibros[0]);
-	
-	string listaNombres[] = {"Efra", "Mario", "Ezequiel", "Tania", "Luis"};
-	string listaApellidos[] = {"Herrera", "Marce", "Ortiz", "Vaca", "Menacho"};
-	
-	//GENERA ESTRUCTURAS Y ASIGNA DATOS ALEATORIOS
-	LIBRO libros[3];
-	for (int i = 0; i < 3; ++i) {
-		libros[i].title = getRandomElement(listaLibros, numLibros);
+// Funciones para generar las estructuras y sus datos
+void generarLibros(LIBRO libros[], string listaLibros[], string listaNombres[], string listaApellidos[]) {
+	for (int i = 0; i < NUM_LIBROS; ++i) {
+		libros[i].title = getRandomElement(listaLibros, NUM_LIBROS);
 		libros[i].author = getRandomElement(listaNombres, 5) + " " + getRandomElement(listaApellidos, 5);
 		libros[i].ISBN = to_string(i + 1) + ",2,3,4,5"; // ISBN falso
 		libros[i].publicationYear = getRandomYear();
 	}
-	
-	USUARIO usuarios[3];
-	for (int i = 0; i < 3; ++i) {
+}
+
+void generarUsuarios(USUARIO usuarios[], string listaNombres[], string listaApellidos[]) {
+	for (int i = 0; i < NUM_USUARIOS; ++i) {
 		usuarios[i].userID = i + 1;
 		usuarios[i].name = getRandomElement(listaNombres, 5);
 		usuarios[i].lastname = getRandomElement(listaApellidos, 5);
 		usuarios[i].email = generateEmail(usuarios[i].name, usuarios[i].lastname);
 	}
-	
-	BIBLIOTECARIO bibliotecarios[3];
-	for (int i = 0; i < 3; ++i) {
+}
+
+void generarBibliotecarios(BIBLIOTECARIO bibliotecarios[], string listaNombres[], string listaApellidos[]) {
+	for (int i = 0; i < NUM_BIBLIOTECARIOS; ++i) {
 		bibliotecarios[i].employeeID = i + 1;
 		bibliotecarios[i].name = getRandomElement(listaNombres, 5);
 		bibliotecarios[i].lastname = getRandomElement(listaApellidos, 5);
 		bibliotecarios[i].email = generateEmail(bibliotecarios[i].name, bibliotecarios[i].lastname);
 	}
-	
-	//UTILIZANDO PUNTEROS PARA PASAR LOS ARRAYS DE LAS ESTRUCCTURAS
-	LIBRO *librosPtr = libros;
-	USUARIO *usuariosPtr = usuarios;
-	BIBLIOTECARIO *bibliotecariosPtr = bibliotecarios;
-	
-	//MUESTRA LOS DATOS GENERADOS
+}
+
+// Función para imprimir las estructuras
+void imprimirLibros(LIBRO libros[]) {
 	cout << "Libros:" << endl;
-	for (int i = 0; i < 3; ++i) {
-		cout << "T�tulo: " << librosPtr[i].title << ", Autor: " << librosPtr[i].author << ", ISBN: " << librosPtr[i].ISBN << ", A�o de Publicaci�n: " << librosPtr[i].publicationYear << endl;
+	for (int i = 0; i < NUM_LIBROS; ++i) {
+		cout << "Título: " << libros[i].title << ", Autor: " << libros[i].author << ", ISBN: " << libros[i].ISBN << ", Año de Publicación: " << libros[i].publicationYear << endl;
 	}
-	
+}
+
+void imprimirUsuarios(USUARIO usuarios[]) {
 	cout << "\nUsuarios:" << endl;
-	for (int i = 0; i < 3; ++i) {
-		cout << "ID: " << usuariosPtr[i].userID << ", Nombre: " << usuariosPtr[i].name << ", Apellido: " << usuariosPtr[i].lastname << ", Email: " << usuariosPtr[i].email << endl;
+	for (int i = 0; i < NUM_USUARIOS; ++i) {
+		cout << "ID: " << usuarios[i].userID << ", Nombre: " << usuarios[i].name << ", Apellido: " << usuarios[i].lastname << ", Email: " << usuarios[i].email << endl;
 	}
-	
+}
+
+void imprimirBibliotecarios(BIBLIOTECARIO bibliotecarios[]) {
 	cout << "\nBibliotecarios:" << endl;
-	for (int i = 0; i < 3; ++i) {
-		cout << "ID: " << bibliotecariosPtr[i].employeeID << ", Nombre: " << bibliotecariosPtr[i].name << ", Apellido: " << bibliotecariosPtr[i].lastname << ", Email: " << bibliotecariosPtr[i].email << endl;
+	for (int i = 0; i < NUM_BIBLIOTECARIOS; ++i) {
+		cout << "ID: " << bibliotecarios[i].employeeID << ", Nombre: " << bibliotecarios[i].name << ", Apellido: " << bibliotecarios[i].lastname << ", Email: " << bibliotecarios[i].email << endl;
 	}
+}
+
+int main() {
+	// Semilla para números aleatorios
+	srand(time(0));
+	
+	// Arrays fijos de datos
+	string listaLibros[] = {"Aprende C", "Aprende PHP", "Aprende Linux", "Aprende C++", "Aprende Ubunto", "Clean Code", "The Art of Computer Programming", "Programming Pearls", "Introduction to Algorithms"};
+	string listaNombres[] = {"Juan", "María", "Pedro", "Ana", "Luis", "Efrain", "Jose", "Carlos", "Cristian"};
+	string listaApellidos[] = {"García", "Martínez", "López", "González", "Rodríguez", "Edmundo", "Flores", "Ponce", "Herrera"};
+	
+	// Generar estructuras
+	LIBRO libros[NUM_LIBROS];
+	USUARIO usuarios[NUM_USUARIOS];
+	BIBLIOTECARIO bibliotecarios[NUM_BIBLIOTECARIOS];
+	
+	// Generar datos aleatorios utilizando punteros
+	generarLibros(libros, listaLibros, listaNombres, listaApellidos);
+	generarUsuarios(usuarios, listaNombres, listaApellidos);
+	generarBibliotecarios(bibliotecarios, listaNombres, listaApellidos);
+	
+	// Imprimir datos utilizando punteros
+	imprimirLibros(libros);
+	imprimirUsuarios(usuarios);
+	imprimirBibliotecarios(bibliotecarios);
 	
 	return 0;
 }
-
