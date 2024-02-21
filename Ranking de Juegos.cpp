22c92e3acc74bcc9f4cb63d90e7e801d7824b847
EfraVHerrera <159298473+EfraVHerrera@users.noreply.github.com>
2024-02-21 12:54:56 -0400

    Update Ranking de Juegos.cpp

--- Ranking de Juegos.cpp
+++ Ranking de Juegos.cpp
@@ -3,7 +3,7 @@
 #include <cstdlib>
 #include <ctime>
 #include <algorithm>
-#include <unordered_set>
+#include <random> // Incluir la cabecera para std::mt19937
 using namespace std;
 
 const int TAMJ = 3;
@@ -14,37 +14,33 @@ struct vjuegos {
 	string nombre;
 	int ranking; // 1 al 10
 	bool tipo; // OFFLINE Y ONLINE;
-	string des; // Descripci髇 del juego
+	string des; // DESCRIPCION
 };
 
 struct persona {
 	string nombre;
 	int id_persona;
 	vjuegos juegos[TAMJ];
-	unordered_set<string> juegosImpresos; // Conjunto para rastrear juegos impresos por esta persona
 };
 
-// Funciones para generar nombres aleatorios
 string generarNombrePersona();
 string generarNombreJuego();
-string generarDescripcionJuego(); // Nueva funci髇 para generar descripciones de juegos
+string generarDescripcionJuego();
 
-void llenarPersona(persona &p);
+void llenarPersona(persona &p, int cant, persona *pArray);
 void llenarJuegos(vjuegos &j);
-void imprimirPersona(persona p, unordered_set<string> &nombresImpresos);
-void imprimirJuegos(vjuegos j, unordered_set<string> &juegosImpresos);
+void imprimirPersona(persona p, int cant, persona *pArray);
+void imprimirJuegos(vjuegos j, bool repetido[], int cant);
 void rankingPorPersona(persona p);
 
 int main() {
 	srand(time(nullptr));
 	
-	persona personas[NUM_PERSONAS]; // Crear un arreglo de personas
+	persona personas[NUM_PERSONAS]; // CREAR ARREGLO DE PERSONA
 	
 	for (int i = 0; i < NUM_PERSONAS; i++) {
-		llenarPersona(personas[i]); // Llenar datos de cada persona
+		llenarPersona(personas[i], i, personas); // LLENAR DATO DE CADA PERSONA
 	}
-	
-	unordered_set<string> nombresImpresos;
 	
 	int opcion;
 	do {
@@ -59,17 +55,17 @@ int main() {
 		switch (opcion) {
 		case 1:
 			for (int i = 0; i < NUM_PERSONAS; i++) {
-				llenarPersona(personas[i]);
+				llenarPersona(personas[i], i, personas); // LLENAR DATO DE CADA PERSONA
 			}
 			break;
 		case 2:
 			for (int i = 0; i < NUM_PERSONAS; i++) {
-				imprimirPersona(personas[i], nombresImpresos);
+				imprimirPersona(personas[i], i, personas); // IMPRIMIR DATOS DE CADA PERSONA
 			}
 			break;
 		case 3:
 			for (int i = 0; i < NUM_PERSONAS; i++) {
-				rankingPorPersona(personas[i]);
+				rankingPorPersona(personas[i]); // MOSTRAR RANKING DE JUEGOS POR PERSONA
 			}
 			break;
 		case 0:
@@ -87,84 +83,110 @@ int main() {
 string generarNombrePersona() {
 	string nombres[] = {"Juan Perez", "Maria Flor", "Noah Ramirez", "Cristian Edmundo", "Efra Herrera", "Thiago", "Santy", "Gerson", "Jose Carlos", "Justin Davis"};
 	static int indice = 0;
-	random_shuffle(nombres, nombres + 10);
+	shuffle(nombres, nombres + 10, mt19937(rand())); // Utilizar std::mt19937
 	return nombres[indice++ % 10];
 }
 
 string generarNombreJuego() {
 	string vjuegos[] = {"Valorant", "Call of Duty: Warzone 2.0", "CSGO", "Counter-Strike: Global Offensive", "League of Legends", "Lost Ark", "Apex Legends", "Final Fantasy XIV", "Fortnite", "Rocket League"};
 	static int indice = 0;
-	random_shuffle(vjuegos, vjuegos + 10);
+	shuffle(vjuegos, vjuegos + 10, mt19937(rand())); // Utilizar std::mt19937
 	return vjuegos[indice++ % 10];
 }
 
 string generarDescripcionJuego() {
 	string descripciones[] = {
-			"Un juego de disparos t醕tico en primera persona.",
-			"Un juego de disparos en equipo con modos de juego variados.",
-			"Un juego de disparos competitivo con una amplia comunidad.",
-			"Un juego de estrategia en tiempo real con elementos de MOBA.",
-			"Un MMORPG 閜ico con una enorme variedad de personajes y habilidades.",
-			"Un juego de rol de acci髇 con un mundo abierto y libertad de exploraci髇.",
-			"Un battle royale con personajes 鷑icos y habilidades especiales.",
-			"Un juego de rol en l韓ea con una historia envolvente y una gran cantidad de contenido.",
-			"Un juego de construcci髇 y supervivencia en un mundo post-apocal韕tico.",
-			"Un juego de f鷗bol donde los jugadores controlan coches con el objetivo de marcar goles."
+		"Un juego En primera persona.",
+			"Un juego en equipo.",
+			"Competitivo con una amplia comunidad.",
+			"Estrategia en tiempo real.",
+			"Variedad de personajes y habilidades.",
+			"Rol de acci贸n con un mundo abierto.",
+			"Un battle royale.",
+			"En l铆nea con una historia envolvente.",
+			"Un juego de construcci贸n y supervivencia.",
+			"F煤tbol donde los jugadores controlan coches."
 	};
 	static int indice = 0;
-	random_shuffle(descripciones, descripciones + 10);
+	shuffle(descripciones, descripciones + 10, mt19937(rand())); // Utilizar std::mt19937
 	return descripciones[indice++ % 10];
 }
 
-void llenarPersona(persona &p) {
-	p.nombre = generarNombrePersona();
-	p.id_persona = rand() % 1000; // Generar un ID aleatorio para la persona
+void llenarPersona(persona &p, int cant, persona *pArray) {
+	string nombre;
+	bool repetido;
+	do {
+		repetido = false;
+		nombre = generarNombrePersona();
+		for (int i = 0; i < cant; ++i) {
+			if (nombre == pArray[i].nombre) {
+				repetido = true;
+				break;
+			}
+		}
+	} while (repetido); // REPETIR HASTA QUE SE GENERA UN NOMBRE 脷NICO
+	p.nombre = nombre;
+	
+	p.id_persona = rand() % 1000; // GENERAR UN ID ALEATORIO PARA LA PERSONA
 	for (int i = 0; i < TAMJ; i++) {
-		llenarJuegos(p.juegos[i]); // Llenar datos de los juegos de la persona
+		llenarJuegos(p.juegos[i]); // LLENAR DATOS DE LOS JUEGOS DE LA PERSONA
 	}
 }
 
 void llenarJuegos(vjuegos &j) {
 	j.nombre = generarNombreJuego();
-	j.ranking = rand() % 10 + 1; // Generar un ranking aleatorio entre 1 y 10
-	j.tipo = rand() % 2; // Generar un tipo aleatorio (0 para OFFLINE, 1 para ONLINE)
-	j.des = generarDescripcionJuego(); // Generar una descripci髇 para el juego
+	j.ranking = rand() % 10 + 1; // GENERAR UN RANKING ALEATORIO ENTRE 1 Y 10
+	j.tipo = rand() % 2; // GENERAR UN TIPO ALEATORIO (0 PARA OFFLINE, 1 PARA ONLINE)
+	j.des = generarDescripcionJuego(); // GENERAR UNA DESCRIPCI脫N PARA EL JUEGO
 }
 
-void imprimirPersona(persona p, unordered_set<string> &nombresImpresos) {
-	if (nombresImpresos.find(p.nombre) != nombresImpresos.end()) {
-		return; // Si el nombre ya se ha impreso, salimos de la funci髇
-	}
+void imprimirPersona(persona p, int cant, persona *pArray) {
+	bool repetido[NUM_JUEGOS] = {false};
 	
 	cout << "Nombre: " << p.nombre << endl;
 	cout << "ID: " << p.id_persona << endl;
 	cout << "Juegos:" << endl;
 	for (int i = 0; i < TAMJ; i++) {
-		imprimirJuegos(p.juegos[i], p.juegosImpresos); // Imprimir datos de los juegos de la persona
+		imprimirJuegos(p.juegos[i], repetido, cant); // IMPRIMIR DATOS DE LOS JUEGOS DE LA PERSONA
 	}
 	cout << endl;
-	
-	nombresImpresos.insert(p.nombre); // Agregar el nombre a los nombres impresos
 }
 
-void imprimirJuegos(vjuegos j, unordered_set<string> &juegosImpresos) {
-	if (juegosImpresos.find(j.nombre) != juegosImpresos.end()) {
-		return;
-	}
-	
+void imprimirJuegos(vjuegos j, bool repetido[], int cant) {
 	cout << "  Nombre del juego: " << j.nombre << endl;
 	cout << "  Ranking: " << j.ranking << "/10" << endl;
 	cout << "  Tipo: " << (j.tipo ? "ONLINE" : "OFFLINE") << endl;
-	cout << "  Descripci髇: " << j.des << endl; // Imprimir la descripci髇 del juego
+	cout << "  Descripci贸n: " << j.des << endl; // IMPRIMIR LA DESCRIPCI脫N DEL JUEGO
 	
-	juegosImpresos.insert(j.nombre); // Agregar el juego a los juegos impresos
+	// Marcar el juego actual como repetido para evitar su impresi贸n en futuras iteraciones
+	for (int i = 0; i < cant; ++i) {
+		if (!repetido[i]) {
+			repetido[i] = true;
+			break; // Salir del bucle una vez que se ha marcado el juego como repetido
+		}
+	}
 }
 
 void rankingPorPersona(persona p) {
 	cout << "Nombre: " << p.nombre << endl;
-	cout << "Ranking de juegos:" << endl;
+	cout << "El Ranking es :" << endl;
+	
+	int maxRanking = 0;
+	string juegoMejorRanking;
+	
 	for (int i = 0; i < TAMJ; i++) {
-		cout << "  " << p.juegos[i].nombre << ": " << p.juegos[i].ranking << "/10" << endl;
+		if (p.juegos[i].ranking > maxRanking) {
+			maxRanking = p.juegos[i].ranking;
+			juegoMejorRanking = p.juegos[i].nombre;
+		}
 	}
+	
+	if (maxRanking > 0) {
+		cout << "  " << juegoMejorRanking << ": " << maxRanking << "/10" << endl;
+	} else {
+		cout << "  Esta persona no tiene juegos registrados." << endl;
+	}
+	
 	cout << endl;
 }
+
